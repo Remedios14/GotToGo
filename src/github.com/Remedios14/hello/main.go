c2965735228c14e41064ec8599717b7ab7bdb2dd
whyi <wanghongyi05@meituan.com>
2021-11-22 13:55:50 +0800

    Finish weather query main

--- src/github.com/Remedios14/hello/main.go
+++ src/github.com/Remedios14/hello/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 // func main() {
@@ -12,51 +14,123 @@ import (
 // }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	mw := multiWeatherProvider{
+		openWeatherMap{},
+		weatherUnderground{apiKey: "your-key-here"},
+	}
 
 	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		begin := time.Now()
 		city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-		data, err := query(city)
+		temp, err := mw.temperature(city)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(data)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"city": city,
+			"temp": temp,
+			"took": time.Since(begin).String(),
+		})
 	})
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello!"))
+type weatherProvider interface {
+	temperature(city string) (float64, error) // in Kelvin, naturally
 }
 
+type multiWeatherProvider []weatherProvider
+
+func (w multiWeatherProvider) temperature(city string) (float64, error) {
+	// Make a channel for temperatures, and a channel for errors
+	// Each provider will push a value into only one
+	temps := make(chan float64, len(w))
+	errs := make(chan error, len(w))
+
+	// For each provider, spawn a goroutine with an anonymous function
+	// That function will invoke the temperature method, and forward the response
+	for _, provider := range w {
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
+	}
+
+	sum := 0.0
+
+	// Collect a temperature or an error from each provider
+	for i := 0; i < len(w); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
+			return 0, err
+		}
+	}
+
+	// Return the average, same as before
+	return sum / float64(len(w)), nil
+}
+
+type openWeatherMap struct{}
+
 // 前一个括号内是入参(指定变量名和类型)，后一个是出参(且仅需指定类型)
-func query(city string) (weatherData, error) {
+func (w openWeatherMap) temperature(city string) (float64, error) {
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?bc17dc9a7a036937af8f566cca3b54a6&q=" + city)
 	// 若有访问错误则返回空信息和错误
 	if err != nil {
-		return weatherData{}, err
+		return 0, err
 	}
 	// 成功响应则关闭响应体
 	defer resp.Body.Close()
 
-	var d weatherData
+	var d struct {
+		Main struct {
+			Kelvin float64 `json:"temp"`
+		} `json:"main"`
+	}
 	// 解码 Get 请求的响应
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
+		return 0, err
 	}
+
+	log.Printf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
 	// 返回信息和空错误
-	return d, nil
+	return d.Main.Kelvin, nil
 }
 
-// type 关键词定义一个新类型为一个 struct
-type weatherData struct {
-	Name string `json:"name"`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
+type weatherUnderground struct {
+	apiKey string
+}
+
+func (w weatherUnderground) temperature(city string) (float64, error) {
+	resp, err := http.Get("http://api.wunderground.com/api/" + w.apiKey + "/conditions/q/" + city + ".json")
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Observation struct {
+			Celsius float64 `json:"temp_c"`
+		} `json:"current_observation"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	kelvin := d.Observation.Celsius + 273.15
+	log.Printf("weatherUnderground: %s: %.2f", city, kelvin)
+	return kelvin, nil
 }
